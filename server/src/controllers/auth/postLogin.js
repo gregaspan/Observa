40e48812ac6669861949c8aa319474c03b10c651
mcaks <miha.caks1@student.um.si>
2024-05-23 18:04:56 +0200

    JWT avtentikacija login logika

--- server/src/controllers/auth/postLogin.js
+++ server/src/controllers/auth/postLogin.js
@@ -1,3 +1,42 @@
+import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export const postLogin = async (req, res) => {
-  return res.send("to je login");
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      //jwt token
+      const token = jwt.sign(
+        //podatki o uporabniku ki jih zelimo enkriptat z JWT token
+        {
+          userId: user._id,
+          email: user.email,
+        },
+        //secret
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "8h",
+        }
+      );
+
+      //poslji response uporabniku
+      return res.status(200).json({
+        userDetails: {
+          email: user.email,
+          token,
+          username: user.username,
+        },
+      });
+    }
+
+    return res.status(400).send("Neveljavni podatki. Poskusi ponovno.");
+  } catch (err) {
+    return res.status(500).send("Nekaj je slo narobe. Poskusi ponovno.");
+  }
 };
