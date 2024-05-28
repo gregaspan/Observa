1ce2eb06066a5b159be7b6fce8e0b1f6f56c3c0b
gregaspan <gregor.span@student.um.si>
2024-05-28 17:46:53 +0200

    react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css'
 import Dashboard from "./components/Dashboard"
+import Login from "./components/Login"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Dashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
