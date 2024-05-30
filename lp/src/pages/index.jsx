57ecaf062f784cc398fff38860af1ed3bed3791c
gregaspan <gregor.span@student.um.si>
2024-05-30 14:39:56 +0200

    landing page

---
+++ lp/src/pages/index.jsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Observa - Transform Your Old Smartphones into a Smart Security System</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
