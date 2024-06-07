65a233242d05839be484b01e2cb6b7163e3e56f0
gregaspan <gregor.span@student.um.si>
2024-06-07 11:00:44 +0200

    profile settings update

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -331,17 +331,18 @@ export default function Example() {
                   </ul>
                 </li>
                 <li className="mt-auto">
-                  <a
-                    href="#"
-                    className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
-                  >
-                    <Cog6ToothIcon
-                      className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
-                      aria-hidden="true"
-                    />
-                    Settings
-                  </a>
-                </li>
+  <button
+    onClick={() => handleNavClick({ name: "Your profile", component: Profile })}
+    className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
+  >
+    <Cog6ToothIcon
+      className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
+      aria-hidden="true"
+    />
+    Settings
+  </button>
+</li>
+
               </ul>
             </nav>
           </div>
